9684f15e1b4bb880a3d97f59157ea2e0371daca9
KIlnourik <kilnur1988@rambler.ru>
2021-07-06 23:22:10 +0500

    Начал писать запрос на добавление существующих данных в сущность post

--- queries.sql
+++ queries.sql
@@ -1,8 +1,21 @@
 INSERT INTO content_type VALUES (NULL, 'Текст', 'text'), (NULL, 'Картинка', 'photo'), (NULL, 'Видео', 'video'), (NULL, 'Ссылка', 'link'), (NULL, 'Цитата', 'quote');
 
-INSERT INTO user VALUES (NULL, CURRENT_TIMESTAMP, 'pikachu@gmail.com', 'pikachu', 'pikapika777', 'img/avatar-pikachu.jpg'), (NULL, CURRENT_TIMESTAMP, 'petr_petrovich@gmail.com', 'petr_petrovich', 'vodka_4ever', 'img/drunk_petrovich.png'), (NULL, CURRENT_TIMESTAMP, 'valiko@yandex.ru', 'valiko', 'mimino555', 'img/valiko_and_larisa_sergeevna.jpg');
+INSERT INTO user (id, email, user_login, user_password, avatar) VALUES (NULL, 'pikachu@gmail.com', 'pikachu', 'pikapika777', 'img/avatar-pikachu.jpg'), (NULL, 'petr_petrovich@gmail.com', 'petr_petrovich', 'vodka_4ever', 'img/drunk_petrovich.png'), (NULL, 'valiko@yandex.ru', 'valiko', 'mimino555', 'img/valiko_and_larisa_sergeevna.jpg');
 
-INSERT INTO comments VALUES (NULL, now(), 'Все супер!', 1, 2), (NULL, now(), 'Ну такое...', 3, 3);
+INSERT INTO post (id, title, author, picture, video, link, user_id, type_id) VALUES
+(NULL, 'Цитата', 'Лариса', '')
+
+INSERT INTO comments (id, comment, user_id, post_id) VALUES
+(NULL, 'Все супер!', 1, 1),
+(NULL, 'Ну такое...', 3, 1),
+(NULL, 'Очень даже неплохо!', 2, 2),
+(NULL, 'Все тлен...', 1, 2),
+(NULL, 'С точки зрения банальной эрудиции...', 3, 3),
+(NULL, 'Всегда когда вижу ЭТО, всегда находят странные мысли...', 2, 3),
+(NULL, 'Вчера казалось лучше', 3, 4),
+(NULL, 'Прелестно, просто прелестно', 1, 4),
+(NULL, 'Успехов тебе!', 2, 5),
+(NULL, 'Отличненько!', 3, 5);
 
 
 
