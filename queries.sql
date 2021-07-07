f50a977aaf8c876b24cf732fdfbbe8d0bf52e9c7
KIlnourik <kilnur1988@rambler.ru>
2021-07-07 11:20:12 +0500

    Выполнил домашнее задание по добавлению данных в БД и составлению запросов из БД, согласно заданию в ДЗ

--- queries.sql
+++ queries.sql
@@ -1,9 +1,12 @@
 INSERT INTO content_type VALUES (NULL, 'Текст', 'text'), (NULL, 'Картинка', 'photo'), (NULL, 'Видео', 'video'), (NULL, 'Ссылка', 'link'), (NULL, 'Цитата', 'quote');
 
-INSERT INTO user (id, email, user_login, user_password, avatar) VALUES (NULL, 'pikachu@gmail.com', 'pikachu', 'pikapika777', 'img/avatar-pikachu.jpg'), (NULL, 'petr_petrovich@gmail.com', 'petr_petrovich', 'vodka_4ever', 'img/drunk_petrovich.png'), (NULL, 'valiko@yandex.ru', 'valiko', 'mimino555', 'img/valiko_and_larisa_sergeevna.jpg');
+INSERT INTO user (id, email, user_login, user_password, avatar) VALUES (NULL, 'pikachu@gmail.com', 'pikachu', 'pikapika777', 'userpic-mark.jpg.jpg'), (NULL, 'petr_petrovich@gmail.com', 'petr_petrovich', 'vodka_4ever', 'userpic.jpg'), (NULL, 'valiko@yandex.ru', 'valiko', 'mimino555', 'userpic-larisa-small.jpg');
 
-INSERT INTO post (id, title, author, picture, video, link, user_id, type_id) VALUES
-(NULL, 'Цитата', 'Лариса', '')
+INSERT INTO post (id, title, text_content, quote_author, watch_count, user_id, type_id) VALUES (NULL, 'Цитата', 'Мы в жизни любим только раз, а после ищем лишь похожих', 'Лариса', 20, 1, 5);
+INSERT INTO post (id, title, text_content, watch_count, user_id, type_id) VALUES (NULL, 'Игра престолов', 'Не могу дождаться начала финального сезона своего любимого сериала!', 12, 3, 1);
+INSERT INTO post (id, title, picture, watch_count, user_id, type_id) VALUES (NULL, 'Наконец, обработал фотки!', 'rock-medium.jpg', 22, 2, 2);
+INSERT INTO post (id, title, picture, watch_count, user_id, type_id) VALUES (NULL, 'Моя мечта', 'coast-medium.jpg', 101, 2, 2);
+INSERT INTO post (id, title, link, watch_count, user_id, type_id) VALUES (NULL, 'Лучшие курсы', 'www.htmlacademy.ru', 45, 1, 4);
 
 INSERT INTO comments (id, comment, user_id, post_id) VALUES
 (NULL, 'Все супер!', 1, 1),
@@ -17,7 +20,12 @@ INSERT INTO comments (id, comment, user_id, post_id) VALUES
 (NULL, 'Успехов тебе!', 2, 5),
 (NULL, 'Отличненько!', 3, 5);
 
+SELECT p.id, p.title, text_content, quote_author, picture, video, link, watch_count, u.user_login, ct.classname FROM post p LEFT JOIN user u ON user_id = u.id LEFT JOIN content_type ct ON type_id = ct.id ORDER BY watch_count;
 
+SELECT p.id, p.title, text_content, quote_author, picture, video, link, watch_count, u.user_login FROM post p LEFT JOIN user u ON user_id = u.id WHERE u.user_login = 'pikachu';
 
+SELECT c.id, comment, u.user_login FROM comments c LEFT JOIN user u ON user_id = u.id WHERE post_id = 1;
 
+INSERT INTO likes VALUES (NULL, 2, 1);
 
+INSERT INTO subscribtions VALUES (NULL, 2, 3);
