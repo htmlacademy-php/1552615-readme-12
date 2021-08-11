a0bd18d06ca76d6db3ae8723e0bef22193344acc
KIlnourik <kilnur1988@rambler.ru>
2021-08-11 22:04:22 +0500

    Начал вторую часть домашнего задания - реализацию сценария показа отдельного поста

--- post.php
+++ post.php
@@ -1,8 +1,10 @@
 <?php
+require('helpers.php');
+require('index.php');
 
 $scriptname = pathinfo(__FILE__, PATHINFO_BASENAME);
 $post_url = "/" . $scriptname . "?";
 
+$post_details = include_template('post-details.php', ['posts' => $sql_posts, 'types' => $sql_types]);
 
-
-$layout = include_template('layout.php');
+$layout = include_template('layout.php', ['popular_content' => $post_details, 'title' => 'readme: публикация', 'is_auth' => $is_auth, 'user_name' => $user_name]);
